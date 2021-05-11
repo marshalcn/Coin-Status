87b1f4aec9f678ddb86ded13f999af7f3e2358e0
madechao <platonic.ma@qq.com>
2021-05-11 17:43:40 +0800

    first commit

---
+++ js/background.js
@@ -0,0 +1,149 @@
+var websocket;
+function createWebSocketConnection() {
+    if('WebSocket' in window){
+        chrome.storage.local.get("instance", function(data) {
+            connect('wss://stream.binance.com:9443/ws/stream');
+        });
+    }
+}
+
+//Make a websocket connection with the server.
+function connect(host) {
+    if (websocket === undefined) {
+        websocket = new WebSocket(host);
+    }
+
+    websocket.onopen = function() {
+        // chrome.storage.local.get(["username"], function(data) {
+        //     websocket.send(JSON.stringify({userLoginId: data.username}));
+        // });
+
+        data = {
+		  "method":"SUBSCRIBE",
+		  "params": [
+		    "rvnusdt@aggTrade",
+		    "shibusdt@aggTrade"
+		  ],
+		  "id": 1
+		};
+		websocket.send(JSON.stringify(data)); //将消息发送到服务端
+    };
+
+    websocket.onmessage = function (event) {
+        // var received_msg = JSON.parse(event.data);
+        // var demoNotificationOptions = {
+        //     type: "basic",
+        //     title: received_msg.subject,
+        //     message: received_msg.message,
+        //     iconUrl: "images/demo-icon.png"
+        // }
+        data = JSON.parse(event.data);
+        var sendData = {
+        };
+		if (data.s == 'SHIBUSDT') {
+			sendData.coin = "SHIB";
+			sendData.price = data.p;
+			// $('#shib').html(data.p);
+		}
+		if (data.s == 'RVNUSDT') {
+			sendData.coin = "RVN";
+			sendData.price = data.p;
+			// $('#rvn').html(data.p);
+		}
+		// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		//     chrome.tabs.sendMessage(tabs[0].id, {action: sendData}, function(response) {});  
+		// });
+		
+		// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		//  chrome.tabs.sendMessage(tabs[0].id, sendData, function(response) { });
+		// });
+
+		// alert(JSON.stringify(sendData));
+		var port = chrome.runtime.connect({name: "coinStatus"});//通道名称
+		port.onDisconnect.addListener(obj => {
+			console.log('disconnected port');
+		});
+
+		port.postMessage(sendData);//发送消息
+		//  port.onMessage.addListener(function(msg) {//监听消息
+		//  	console.log(msg);
+		//    // if (msg.question == "Who's there?")
+		//    //   port.postMessage({answer: "yisheng"});
+		//    // else if (msg.question == "Madame who?")
+		//    //   port.postMessage({answer: "Madame... Bovary"});
+		// });
+		// chrome.runtime.onMessage.addListener(function (request,sender,callback) {
+		// 	console.log(request);
+		// 	console.log(sender);
+		//     callback(sendData);
+		// });
+		// chrome.notifications.create("", sendData);
+        // updateData();
+    };
+
+    //If the websocket is closed but the session is still active, create new connection again
+    websocket.onclose = function() {
+        websocket = undefined;
+        chrome.storage.local.get(['demo_session'], function(data) {
+            if (data.demo_session) {
+                createWebSocketConnection();
+            }
+        });
+    };
+}
+
+//Close the websocket connection
+function closeWebSocketConnection(username) {
+    if (websocket != null || websocket != undefined) {
+        websocket.close();
+        websocket = undefined;
+    }
+}
+
+
+function start(){
+	var ws = new WebSocket("wss://stream.binance.com:9443/ws/stream"); 
+	//申请一个WebSocket对象，参数是服务端地址，同http协议使用http://开头一样，WebSocket协议的url使用ws://开头，另外安全的WebSocket协议使用wss://开头
+	ws.onopen = function(){
+	   data = {
+		  "method":"SUBSCRIBE",
+		  "params": [
+		    "rvnusdt@aggTrade",
+		    "shibusdt@aggTrade"
+		  ],
+		  "id": 1
+		};
+	　　ws.send(JSON.stringify(data)); //将消息发送到服务端
+	}
+	ws.onmessage = function(e){
+		console.log(e.data)
+		data = JSON.parse(e.data);
+		if (data.s == 'SHIBUSDT') {
+			$('#shib').html(data.p);
+		}
+		if (data.s == 'RVNUSDT') {
+			$('#rvn').html(data.p);
+		}
+		console.log(data.p);
+	　　//当客户端收到服务端发来的消息时，触发onmessage事件，参数e.data包含server传递过来的数据
+	// 　　console.log(e.data);
+	}
+	ws.onclose = function(e){
+		req = {
+		  "method": "UNSUBSCRIBE",
+		  "params": [
+		    "rvnusdt@aggTrade",
+		    "shibusdt@aggTrade"
+		  ],
+		  "id": 1
+		}
+
+		ws.send(JSON.stringify(data));
+	　　//当客户端收到服务端发送的关闭连接请求时，触发onclose事件
+	　　console.log("close");
+	}
+	ws.onerror = function(e){
+	　　//如果出现连接、处理、接收、发送数据失败的时候触发onerror事件
+	　　console.log(error);
+	}
+};
---
+++ js/main.js
@@ -0,0 +1,100 @@
+// $('#search').bind('keydown', function(){
+// 	$('#val').html($('#search').val());
+// });
+$('#search').bind('keyup', function(){
+	$('#val').html($('#search').val());
+});
+$('#search').bind('change', function(){
+	console.log("hello");
+	console.log($('#search').val());
+	$('#val').html($('#search').val());
+});
+
+// function updateData(){
+// 	chrome.notifications
+// };
+
+// chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+// 	alert(msg.action);
+//    // if (msg.action == 'SendIt') {
+//    //    alert("Message recieved!");
+//    // }
+// });
+
+// chrome.runtime.onMessage.addListener(function (request,sender,callback) {
+// 	console.log(request);
+// 	console.log(sender);
+//     callback('ok');
+// });
+// chrome.runtime.sendMessage("sendData", function (response) {
+//     console.log('content get response:',response);
+// });
+
+// chrome.runtime.onMessage.addListener(
+//    function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//                 "from a content script:" + sender.tab.url :
+//                "from the extension");
+//      // if (request.greeting == "hello")//判断是否为要处理的消息
+//      //        sendResponse({farewell: "goodbye"});
+//      console.log(request);
+// });
+
+
+chrome.runtime.onConnect.addListener((port)=> {
+    console.assert(port.name == "coinStatus");
+    // if (port.name != "coinStatus") {
+    // 	return;
+    // }
+    port.onDisconnect.addListener(obj => {
+		console.log('disconnected port');
+	});
+    port.onMessage.addListener((msg)=> {
+    	// alert($('#shib'));
+    	// alert($('#shib').val());
+    	// alert(msg.price);
+    	// alert(JSON.stringify(msg));
+    	if (msg.coin == 'SHIB') {
+			// sendData.coin = "SHIB";
+			// sendData.price = data.p;
+			$('#shib').html(msg.price);
+		}
+		if (msg.coin == 'RVN') {
+			// sendData.coin = "RVN";
+			// sendData.price = data.p;
+			$('#rvn').html(msg.price);
+		}
+      // console.log(msg);
+   });
+});
+
+//  var port = chrome.runtime.connect({name: "coinStatus"});//通道名称
+//  // port.postMessage({joke: "Knock knock"});//发送消息
+//  port.onMessage.addListener(function(msg) {//监听消息
+//  	console.log(msg);
+//    // if (msg.question == "Who's there?")
+//    //   port.postMessage({answer: "yisheng"});
+//    // else if (msg.question == "Madame who?")
+//    //   port.postMessage({answer: "Madame... Bovary"});
+// });
+
+window.onload = function(){
+	var bg = chrome.extension.getBackgroundPage();
+	bg.createWebSocketConnection();
+	
+};
+window.onbeforeunload = function() {
+	chrome.runtime.Port.disconnect();
+}
+function myrefresh() {
+	window.location.reload();
+}
+//指定1秒刷新一次
+// setTimeout('myrefresh()', 1000);
+
+// new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello Vue.js!'
+//   }
+// })
